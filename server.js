b3ec898469ef3b4868dc5123b83d675eca55f5d1
altr-r <arafatrahman0012@gmail.com>
2025-02-11 18:08:59 +0600

    created a server using express and learned about get and post request

--- server.js
+++ server.js
@@ -1,18 +1,56 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("<h1>Hello World</h1>");
-    res.statusCode = 200;
-    res.end();
-  } 
-  else {
-    res.write("<h1>404 Not Found</h1>");
-    res.statusCode = 200;
-    res.end();
+const app = express();
+app.use(express.json());
+
+const books = [
+  {
+    id: "1",
+    name: "Personal Finance",
+    price: 500,
+  },
+  {
+    id: "2",
+    name: "JavaScript for Dummies",
+    price: 1500,
+  },
+  {
+    id: "2",
+    name: "JavaScript for Dummies Part 2",
+    price: 1200,
+  },
+  {
+    id: "2",
+    name: "JavaScript for Dummies Part 3",
+    price: 300,
+  },
+  {
+    id: "2",
+    name: "JavaScript for Dummies Part 4",
+    price: 1800,
+  },
+];
+
+app.get("/books", (req, res) => {
+  if (req.query.show === "all") {
+    return res.json(books);
   }
+
+  if (req.query.price == "500") {
+    const result = books.filter((book) => book.price <= 500);
+    return res.json(result);
+  }
+
+  return res.json(books);
 });
 
-server.listen(8000, () => {
+app.post("/books", (req, res) => {
+  const book = req.body;
+  books.push(book);
+
+  res.json(books);
+});
+
+app.listen("8000", () => {
   console.log("Server is listening on port 8000");
 });
